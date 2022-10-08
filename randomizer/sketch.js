bf40f56dd83a736f5e4b5fa5c2ee20f0b32980b9
Joyjobobo <joypanyj@gmail.com>
2022-10-07 18:30:58 -0700

    2b

--- randomizer/sketch.js
+++ randomizer/sketch.js
@@ -1,23 +1,40 @@
-let colors = ["red","orange","yellow","green","blue","purple"];
+let colors = [{
+  name:"red",
+  color: "rose"
+},{
+  name:"orange",
+  color:"juice"
+},{
+  name:"yellow",
+  color:"banana"
+},{
+  name:"green",
+  color:"grass"
+},{
+  name:"blue",
+  color:"sky"
+},{
+  name:"purple",
+  color:"lavender"
+
+}];
+
+let randomIndex;
+
 
 function setup(){
   createCanvas(600,600);
   background(200);
 
-  console.log("initial array is ");
-  console.log(colors);
-
-
-  colors.pop();
-  console.log("array after pop");
-  console.log(colors);
-
-  colors.push();
-  console.log("array after push");
-  console.log(colors);
-
 }
 
 function draw(){
 
+}
+
+function mousePressed(){
+  background(random(200,240));
+  randomIndex = int(random(colors.length));
+  text(colors[randomIndex].name,50,50);
+  colors.splice(randomIndex,1);
 }
